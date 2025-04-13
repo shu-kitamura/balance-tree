c3a8ef8d389722aea5013fed549b0216dcb25865
shu-kitamura <shusei3316@yahoo.co.jp>
2025-04-13 23:20:12 +0900

    Change Class Node -> LeafNode

--- b_tree/node.py
+++ b_tree/node.py
@@ -1,11 +1,18 @@
-class Node:
-    def __init__(self, min: int, is_leaf: bool = True):
-        self.is_leaf: bool = is_leaf # If the node is a leaf.
-        self.keys: list = [] # The keys in the node.
-        self.children: list = [] # The children of the node.
-        self.min_num_keys: int = min # The minimum number of keys in the node.
+class LeafNode:
+    """A class representing a leaf node in a B-tree."""
+
+    def __init__(self, min: int):
+        self.kv_pairs: dict = {}
+        self.min_num_keys: int = min  # The minimum number of keys in the node.
 
     def __str__(self):
-        return (
-            f"Node(is_leaf={self.is_leaf}, keys={self.keys}, num_keys={self.num_keys})"
-        )
+        return f"LeafNode(is_leaf={self.is_leaf}, keys={self.keys}, num_keys={self.num_keys})"
+
+    def insert(self, key: int, value: int):
+        """Insert a key-value pair into the leaf node."""
+
+        self.kv_pairs[key] = value
+        # TODO: split the node
+        if len(self.kv_pairs) > (2 * self.min_num_keys) - 1:
+            # self.split()
+            pass
--- tests/test_node.py
+++ tests/test_node.py
@@ -1,16 +1,19 @@
-def test_node_init():
-    from b_tree.node import Node
+from b_tree.node import LeafNode
 
-    # Test case 1: Initialize a leaf node with min_num_keys = 2
-    leaf_node: Node = Node(min=2, is_leaf=True)
-    assert leaf_node.is_leaf
-    assert leaf_node.keys == []
-    assert leaf_node.children == []
-    assert leaf_node.min_num_keys == 2
 
-    # Test case 2: Initialize a non-leaf node with min_num_keys = 3
-    non_leaf_node: Node = Node(min=3, is_leaf=False)
-    assert not non_leaf_node.is_leaf
-    assert non_leaf_node.keys == []
-    assert non_leaf_node.children == []
-    assert non_leaf_node.min_num_keys == 3
+def test_leaf_node_init():
+    node: LeafNode = LeafNode(2)
+    assert node.kv_pairs == {}
+    assert node.min_num_keys == 2
+
+
+def test_leaf_node_insert():
+    node: LeafNode = LeafNode(2)
+    node.insert(1, 1)
+    assert node.kv_pairs == {1: 1}
+    node.insert(2, 2)
+    assert node.kv_pairs == {1: 1, 2: 2}
+    node.insert(3, 3)
+    assert node.kv_pairs == {1: 1, 2: 2, 3: 3}
+    node.insert(4, 4)
+    assert node.kv_pairs == {1: 1, 2: 2, 3: 3, 4: 4}

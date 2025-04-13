835fe72f2b7933eec216d7985ed532c8c9576e93
shu-kitamura <shusei3316@yahoo.co.jp>
2025-04-13 22:51:37 +0900

    add comment

--- b_tree/node.py
+++ b_tree/node.py
@@ -1,9 +1,9 @@
 class Node:
     def __init__(self, min: int, is_leaf: bool = True):
-        self.is_leaf: bool = is_leaf
-        self.keys: list = []
-        self.children: list = []
-        self.min_num_keys: int = min
+        self.is_leaf: bool = is_leaf # If the node is a leaf.
+        self.keys: list = [] # The keys in the node.
+        self.children: list = [] # The children of the node.
+        self.min_num_keys: int = min # The minimum number of keys in the node.
 
     def __str__(self):
         return (

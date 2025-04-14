25fb546f78fd0733effa2df21297b51c7d557790
shu-kitamura <shusei3316@yahoo.co.jp>
2025-04-14 20:30:20 +0900

    Change min -> max:

--- b_tree/node.py
+++ b_tree/node.py
@@ -6,9 +6,9 @@ T = TypeVar("T")
 class LeafNode(Generic[T]):
     """A class representing a leaf node in a B-tree."""
 
-    def __init__(self, min: int) -> None:
+    def __init__(self, max: int) -> None:
         self.kv_pairs: dict[T, int] = {}
-        self.min_num_keys: int = min  # The minimum number of keys in the node.
+        self.max_num_keys: int = max  # The maximum number of keys in the node.
 
     def __str__(self) -> str:
         return f"LeafNode({self.kv_pairs}, min_num_keys={self.min_num_keys})"
@@ -18,6 +18,6 @@ class LeafNode(Generic[T]):
 
         self.kv_pairs[key] = value
         # TODO: split the node
-        if len(self.kv_pairs) > (2 * self.min_num_keys) - 1:
+        if len(self.kv_pairs) > self.max_num_keys:
             # self.split()
             pass

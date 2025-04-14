9b5fc50507c69d5bbeae4525ee90b6ed8c3b02c0
shu-kitamura <shusei3316@yahoo.co.jp>
2025-04-14 21:18:04 +0900

    Add and Fix tests

--- tests/test_node.py
+++ tests/test_node.py
@@ -3,20 +3,19 @@ from b_tree.node import KeyValuePair, LeafNode
 
 def test_leaf_node_init():
     node: LeafNode = LeafNode(2)
-    assert node.kv_pairs == {}
+    assert node.kv_pairs == []
     assert node.max_num_keys == 2
 
 
 def test_leaf_node_insert():
     node: LeafNode = LeafNode(2)
-    node.insert(1, 1)
-    assert node.kv_pairs == {1: 1}
-    node.insert(2, 2)
-    assert node.kv_pairs == {1: 1, 2: 2}
-    node.insert(3, 3)
-    assert node.kv_pairs == {1: 1, 2: 2, 3: 3}
-    node.insert(4, 4)
-    assert node.kv_pairs == {1: 1, 2: 2, 3: 3, 4: 4}
+    node.insert(1, 100)
+    assert node.kv_pairs[0].key == 1
+    assert node.kv_pairs[0].value == 100
+
+    node.insert(2, 200)
+    assert node.kv_pairs[1].key == 2
+    assert node.kv_pairs[1].value == 200
 
 
 def test_leaf_node_get():
@@ -27,6 +26,14 @@ def test_leaf_node_get():
     node.insert(2, 2)
     assert node.get(2) == 2
 
+def test_leaf_node_keys():
+    node: LeafNode = LeafNode(5)
+    node.insert(1, 1)
+    node.insert(2, 2)
+    assert node.keys() == [1, 2]
+
+    node.insert(3, 3)
+    assert node.keys() == [1, 2, 3]
 
 def test_key_value_pair_init():
     kv_pair = KeyValuePair(1, 1)

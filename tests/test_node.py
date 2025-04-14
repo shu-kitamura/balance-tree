3342065eb69c8e70f94658d7873f87004f26d023
shu-kitamura <shusei3316@yahoo.co.jp>
2025-04-14 21:31:25 +0900

    Change LeafNode -> Node
    LeafNode と InternalNode を分ける必要がなさそうだったので変更した。
    それに伴って insert などの関数は消してしまった。

--- tests/test_node.py
+++ tests/test_node.py
@@ -1,39 +1,5 @@
-from b_tree.node import KeyValuePair, LeafNode
+from b_tree.node import KeyValuePair, Node
 
-
-def test_leaf_node_init():
-    node: LeafNode = LeafNode(2)
-    assert node.kv_pairs == []
-    assert node.max_num_keys == 2
-
-
-def test_leaf_node_insert():
-    node: LeafNode = LeafNode(2)
-    node.insert(1, 100)
-    assert node.kv_pairs[0].key == 1
-    assert node.kv_pairs[0].value == 100
-
-    node.insert(2, 200)
-    assert node.kv_pairs[1].key == 2
-    assert node.kv_pairs[1].value == 200
-
-
-def test_leaf_node_get():
-    node: LeafNode = LeafNode(5)
-    node.insert(1, 1)
-    assert node.get(1) == 1
-    assert node.get(2) is None
-    node.insert(2, 2)
-    assert node.get(2) == 2
-
-def test_leaf_node_keys():
-    node: LeafNode = LeafNode(5)
-    node.insert(1, 1)
-    node.insert(2, 2)
-    assert node.keys() == [1, 2]
-
-    node.insert(3, 3)
-    assert node.keys() == [1, 2, 3]
 
 def test_key_value_pair_init():
     kv_pair = KeyValuePair(1, 1)
@@ -43,3 +9,11 @@ def test_key_value_pair_init():
     kv_pair = KeyValuePair("key", 100)
     assert kv_pair.key == "key"
     assert kv_pair.value == 100
+
+
+def test_node_init():
+    node = Node(2)
+    assert node.is_leaf is True
+    assert node.t == 2
+    assert node.items == []
+    assert node.children == []

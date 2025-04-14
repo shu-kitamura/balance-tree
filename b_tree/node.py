2fe9919e4662114b74ca18551240d7c3a8603caf
shu-kitamura <shusei3316@yahoo.co.jp>
2025-04-14 21:48:47 +0900

    add Insert leaf node

--- b_tree/node.py
+++ b_tree/node.py
@@ -20,5 +20,22 @@ class Node(Generic[T]):
     def __init__(self, is_leaf: bool, t: int):
         self.items: list[KeyValuePair] = []
         self.children: list[Node] = []
-        self.is_leaf: bool = is_leaf
+        self.is_leaf = is_leaf
         self.t = t
+
+    def insert(self, kv_pair: KeyValuePair[T]) -> None:
+        """Insert a key-value pair into the node."""
+
+        if self.is_leaf:
+            self._insert_to_leaf(kv_pair)
+        else:
+            # TODO: Implement the logic for inserting into a non-leaf node.
+            pass
+    
+    def _insert_to_leaf(self, kv_pair: KeyValuePair[T]) -> None:
+        """Insert a key-value pair into a leaf node."""
+        i = len(self.items) - 1
+        while i >= 0 and kv_pair.key < self.items[i].key:
+            i -= 1
+
+        self.items.insert(i + 1, kv_pair)

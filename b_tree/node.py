daf3bba87573c7efd228616dbd55a25d222bc997
shu-kitamura <shusei3316@yahoo.co.jp>
2025-04-14 21:01:13 +0900

    Add KeyValuePair class

--- b_tree/node.py
+++ b_tree/node.py
@@ -3,25 +3,38 @@ from typing import Generic, TypeVar
 T = TypeVar("T")
 
 
+class KeyValuePair(Generic[T]):
+    """A class representing a key-value pair."""
+
+    def __init__(self, key: T, value: int) -> None:
+        self.key = key  # The key of the pair.
+        self.value = value  # The value of the pair.
+
+    def __str__(self) -> str:
+        return f"KeyValuePair({self.key}, {self.value})"
+
+
 class LeafNode(Generic[T]):
     """A class representing a leaf node in a B-tree."""
 
     def __init__(self, max: int) -> None:
-        self.kv_pairs: dict[T, int] = {}
+        self.kv_pairs: list[KeyValuePair] = []
         self.max_num_keys: int = max  # The maximum number of keys in the node.
 
     def __str__(self) -> str:
-        return f"LeafNode({self.kv_pairs}, min_num_keys={self.min_num_keys})"
+        return f"LeafNode({self.kv_pairs}, min_num_keys={self.max_num_keys})"
 
     def insert(self, key: T, value: int) -> None:
         """Insert a key-value pair into the leaf node."""
+        i = 0
+        while i < len(self.kv_pairs) and self.kv_pairs[i].key < key:
+            i += 1
 
-        self.kv_pairs[key] = value
-        # TODO: split the node
-        if len(self.kv_pairs) > self.max_num_keys:
-            # self.split()
-            pass
+        self.kv_pairs.insert(i, KeyValuePair(key, value))
 
     def get(self, key: T) -> int | None:
         """Get the value associated with a key in the leaf node."""
-        return self.kv_pairs.get(key, None)
+        for kv_pair in self.kv_pairs:
+            if kv_pair.key == key:
+                return kv_pair.value
+        return None
